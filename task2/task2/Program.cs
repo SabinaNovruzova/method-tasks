c6eb60856c5abc7452b8cc60d2a6c84130e424b8
Sabina Novruzova <tu7cldm19@aztu.local>
2022-03-10 13:46:49 +0400

    first commit

---
+++ task2/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { -5, -10, -30, -40, -55, -67, -89, -90 };
+            Positive(ref arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        static void Positive(ref int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<0)
+                {
+                    arr[i]=arr[i] * -1;
+                }
+            }
+        }
+    }
+}
